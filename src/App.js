3521a6d23c5cd7aac993400dc1751d85b3f422bd
Elnar <elnarzulfuqarli2001@gmail.com>
2023-07-23 11:06:46 +0400

    Api ve Delete click islemi hazirdir

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import {Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Note the use of BrowserRouter as Router
 
 import Home from './routes/Home'
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path={'/'} element={<Home />}></Route>
-      </Routes>
+      <Router> {/* Use the Router component to wrap your Routes */}
+        <Routes>
+          <Route path={'/'} element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,55 @@
 import React from 'react'
 import './TableStyle.css';
 
+import { useState,useEffect } from 'react';
+
 function Table() {
+
+    
+
+    const [items, setData] = useState([]);
+    
+    useEffect (() => {
+        fetch('http://localhost:3000/items')
+        .then(response => response.json())
+        .then(items => setData(items))
+
+        }, []);
+
+    const handleDelete = (id) => {
+        
+        setData((prevItems) => prevItems.filter((item) => item.id !== id));
+    };
+
+    const result = items.map((item) => {
+    return(
+        <div key={item.id} className='table-body'>
+                <hr className='table-line-two' />
+                <div>
+                    <p>{item.firstName}</p>
+                    <p>{item.lastName}</p>
+                    <p>{item.category}</p>
+                    <p>{item.date}</p>
+                    <button onClick={() => handleDelete(item.id)}>Delete</button>
+                </div>
+            </div>
+    )
+    })
     return (
-        <div>Table</div>
+        <form>
+            <h1>My Info</h1>
+            <hr className='table-line-one' />
+            <div className='table-heading'>
+                <h3>FirstName</h3>
+                <h3>LastName</h3>
+                <h3>Category</h3>
+                <h3>Date</h3>
+                <div></div>
+            </div>
+            
+            {result}
+
+        </form>
     )
 }
 
